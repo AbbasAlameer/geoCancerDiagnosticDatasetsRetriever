d8232aa0c26a305f2ac54aa85f565d0ba07c0d64
AbbasAlameer <85877376+AbbasAlameer@users.noreply.github.com>
2021-11-04 00:11:09 +0300

    Add files via upload

--- lib/App/geoCancerDiagnosticDatasetsRetriever.pm
+++ lib/App/geoCancerDiagnosticDatasetsRetriever.pm
@@ -1,1 +1,28 @@
+package App::geoCancerDiagnosticDatasetsRetriever;
 
+our $VERSION = 1.00;
+
+=head1 NAME
+
+App::geoCancerDiagnosticDatasetsRetriever - GEO Cancer Diagnostic Datasets Retriever is a bioinformatics tool for cancer diagnostic dataset retrieval from the GEO website.
+
+=head1 DESCRIPTION
+
+See F<bin/geoCancerDiagnosticDatasetsRetriever> for details of the application.
+
+=head1 AUTHOR
+
+Abbas Alameer (Bioinformatics and Molecular Modelling Group, Kuwait University), in collaboration with Davide Chicco (University of Toronto)
+
+For information, please contact Abbas Alameer at abbas.alameer(AT)ku.edu.kw
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2021 by Abbas Alameer, Kuwait University
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2 (GPLv2).
+
+=cut
+
+1;
