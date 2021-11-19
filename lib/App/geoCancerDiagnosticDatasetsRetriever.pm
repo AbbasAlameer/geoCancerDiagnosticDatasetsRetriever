df0a523037be56e74ca48dcb2198d41c937e0bdd
AbbasAlameer <85877376+AbbasAlameer@users.noreply.github.com>
2021-11-19 17:14:26 +0300

    Latest update

--- lib/App/geoCancerDiagnosticDatasetsRetriever.pm
+++ lib/App/geoCancerDiagnosticDatasetsRetriever.pm
@@ -12,13 +12,13 @@ See F<bin/geoCancerDiagnosticDatasetsRetriever> for details of the application.
 
 =head1 AUTHOR
 
-Abbas Alameer (Bioinformatics and Molecular Modelling Group, Kuwait University), in collaboration with Davide Chicco (University of Toronto)
+Abbas Alameer (Kuwait University) and Davide Chicco (University of Toronto)
 
-For information, please contact Abbas Alameer at abbas.alameer(AT)ku.edu.kw
+For information, please contact Abbas Alameer at abbas.alameer(AT)ku.edu.kw or Davide Chicco at davidechicco(AT)davidechicco.it
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2021 by Abbas Alameer, Kuwait University
+Copyright 2021 by Abbas Alameer (Kuwait University) and Davide Chicco (University of Toronto)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2 (GPLv2).
